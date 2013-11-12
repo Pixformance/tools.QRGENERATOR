5918e6a59ed198597ee1732eaed49aad02398f45
Tomasz Naumowicz <tomasz@naumowicz.de>
2013-11-13 00:58:25 +0100

    POC2: using iTextSharp 4.1.6, first documents

--- Tags/POC2_PDF_iTextSharp/Form1.Designer.cs
+++ Tags/POC2_PDF_iTextSharp/Form1.Designer.cs
@@ -28,20 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Output file:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(77, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(586, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::POC2_PDF_iTextSharp.Properties.Resources.qrcode;
+            this.pictureBox1.Location = new System.Drawing.Point(16, 80);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(250, 250);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(570, 389);
+            this.ClientSize = new System.Drawing.Size(675, 389);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- Tags/POC2_PDF_iTextSharp/Form1.cs
+++ Tags/POC2_PDF_iTextSharp/Form1.cs
@@ -7,6 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+
+using iTextSharp.text;
+using iTextSharp.text.pdf;
 
 namespace POC2_PDF_iTextSharp
 {
@@ -15,6 +19,51 @@ namespace POC2_PDF_iTextSharp
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = Path.Combine(Path.GetDirectoryName(Application.ExecutablePath), "out.pdf");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(textBox1.Text))
+            {
+                if (DialogResult.No == MessageBox.Show("Output file exists, delete?", "Outch!", MessageBoxButtons.YesNo))
+                    return;
+
+                File.Delete(textBox1.Text);
+            }
+
+            BaseFont bfTimes = BaseFont.CreateFont(BaseFont.HELVETICA, BaseFont.CP1252, true);
+            iTextSharp.text.Font times = new iTextSharp.text.Font(bfTimes, 8, iTextSharp.text.Font.NORMAL, iTextSharp.text.Color.LIGHT_GRAY);
+
+
+            Document doc = new Document(PageSize.A4);
+            PdfWriter.GetInstance(doc, new FileStream(textBox1.Text, FileMode.Create));
+
+            doc.Open();
+
+            Paragraph heading = new Paragraph("Page Heading", new iTextSharp.text.Font(iTextSharp.text.Font.HELVETICA, 28f, iTextSharp.text.Font.BOLD));
+            heading.SpacingAfter = 18f;
+            doc.Add(heading);
+
+            MultiColumnText columns = new MultiColumnText();
+            //float left, float right, float gutterwidth, int numcolumns
+            columns.AddRegularColumns(36f, doc.PageSize.Width - 36f, 24f, 10);
+            Paragraph para = new Paragraph("qr code", times);
+            para.SpacingAfter = 9f;
+            para.Alignment = Element.ALIGN_JUSTIFIED;
+            for (int i = 0; i < 40; i++)
+            {
+                columns.AddElement(para);
+            }
+
+            doc.Add(columns);
+
+            doc.Close();
+
         }
     }
 }
