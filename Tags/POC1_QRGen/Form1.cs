61636d4baa86b3b648dbbac6747d3744792440dd
Tomasz Naumowicz <tomasz@naumowicz.de>
2013-11-12 23:58:21 +0100

    POC1: QR generation with ZXing for C#

--- Tags/POC1_QRGen/Form1.cs
+++ Tags/POC1_QRGen/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ZXing;
+using ZXing.Rendering;
 
 namespace POC1_QRGen
 {
@@ -15,6 +17,28 @@ namespace POC1_QRGen
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var writer = new BarcodeWriter
+                {
+                    Format = BarcodeFormat.QR_CODE,
+                    Options = new ZXing.Common.EncodingOptions
+                    {
+                        Height = 200,
+                        Width = 200
+                    },
+                    Renderer = (IBarcodeRenderer<Bitmap>)Activator.CreateInstance(typeof(BitmapRenderer))
+                };
+                pictureBox1.Image = writer.Write(textBox1.Text);
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(this, exc.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
